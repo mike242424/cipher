c0ad22b5d5c726efff1f4da11e5078195725fd8f
Michael Byers <mikebyers24@gmail.com>
2024-07-05 14:59:42 -0400

    refactored ceasar function.

--- main.py
+++ main.py
@@ -4,60 +4,40 @@ print("Welcome to the cipher program.")
 
 go_again = True
 
-def encrypt(input, shift):
+def ceasar(input, shift, type):
   cipher_message = ''
   for letter in input:
     old_index = alphabet.index(letter)
-    new_index = (old_index + shift) % len(alphabet)
-    cipher_message += alphabet[new_index]
-  return cipher_message
-
-def decrypt(input, shift):
-  cipher_message = ''
-  for letter in input:
-    old_index = alphabet.index(letter)
-    new_index = (old_index - shift) % len(alphabet)
+    if(type == 'encode'):
+      new_index = (old_index + shift) % len(alphabet)
+    elif(type == 'decode'):
+      new_index = (old_index - shift) % len(alphabet)
     cipher_message += alphabet[new_index]
   return cipher_message
 
 while(go_again == True):
   selection = input("Type 'encode' to encrypt or 'decode' to decrypt:\n")
 
+  message = input("Type your message:\n").lower()
+  shift_number = int(input("Type the shift number:\n"))
+
+
   if(selection == 'encode'):
-    message = input("Type your message:\n").lower()
-    shift_number = int(input("Type the shift number:\n"))
-
-    encrypted_message = encrypt(input = message, shift = shift_number)
-
+    encrypted_message = ceasar(input = message, shift = shift_number, type = 'encode')
     print(f"Here's the encoded result: {encrypted_message}")
-
-    repeat = input("Type 'yes' if you want to go again. Otherwise type 'no.\n")
-
-    if(repeat == 'no'):
-      go_again = False
-    elif(repeat == 'yes'):
-      go_again = True
-    else:
-      print('Invalid answer.')
-      go_again = False
-
   elif(selection == 'decode'):
-    message = input("Type your message:\n")
-    shift_number = int(input("Type the shift number:\n"))
-
-    decrypted_message = decrypt(input = message, shift = shift_number)
-
+    decrypted_message = ceasar(input = message, shift = shift_number, type = "decode")
     print(f"Here's the decoded result: {decrypted_message}")
-
-    repeat = input("Type 'yes' if you want to go again. Otherwise type 'no.\n")
-
-    if(repeat == 'no'):
-      go_again = False
-    elif(repeat == 'yes'):
-      go_again = True
-    else:
-      print('Invalid answer.')
-      go_again = False
-
   else:
     print('Invalid input')
+
+  repeat = input("Type 'yes' if you want to go again. Otherwise type 'no.\n")
+
+  if(repeat == 'no'):
+    go_again = False
+  elif(repeat == 'yes'):
+    go_again = True
+  else:
+    print('Invalid answer.')
+    go_again = False
+
